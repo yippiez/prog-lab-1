96ab2f84da3ec7d44fcdedda6bc7a5985432c6f2
yippiez <erencemayez@gmail.com>
2020-10-21 19:42:32 +0300

    Formatting

--- main.c
+++ main.c
@@ -2,37 +2,39 @@
 #include <math.h>
 #include <stdio.h>
 
-// circle(x, y, r) r -> yarıçap
-// line(x1, y1, x2, y2) x1,y1 ilk noktanın kordinatı x2,y2 inkincinin kordinatı
-
-// nokta verileri bu structa tutulur
-typedef struct Nokta{
+// struct for holding the point value
+typedef struct Point{
     float x;
     float y;
-} nokta;
+} point;
 
-// iki nokta arası uzunluğu hesaplar
-double uzunluk(nokta a, nokta b){
+
+// calculate the distancce between two points
+double distance(point a, point b){
     return sqrt(pow((a.x-b.x),2)+pow((a.y-b.y),2));
 }
 
-// nokta ciz
-void draw_point(nokta p, nokta merkez){
-    circle((merkez.x + p.x), (merkez.y - p.y), 3);
+
+// draw a point with fixed radius (3)
+void draw_point(point p, point center_point){
+    circle((center_point.x + p.x), (center_point.y - p.y), 3);
 }
 
-void hesapla(int n, nokta *liste, nokta merkez){
-    double max=0;
-    nokta tmp;
-    nokta tmp2;
 
+// this calculates and prints a circle big enough to include all points
+void calculate(int n, point *liste, point center_point){
+    double max=0; // max distance between any point
+    point tmp;
+    point tmp2;
+
+    // iterate over every 2 combination of points and check their distances
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
 
             if(i==j)
                 continue;
 
-            double u = uzunluk(liste[i], liste[j]);
+            double u = distance(liste[i], liste[j]);
             if(u>max){
                 max = u;
                 tmp = liste[i];
@@ -41,40 +43,42 @@ void hesapla(int n, nokta *liste, nokta merkez){
         }
     }
     printf("%lf maks deger.\n", max);
-    nokta daireMerkezi = {(tmp.x+tmp2.x)/2, (tmp.y+tmp2.y)/2};
-    printf("Daire kordinatları: %f, %f\n", daireMerkezi.x, daireMerkezi.y);
-    circle(merkez.x + daireMerkezi.x, merkez.y - daireMerkezi.y, max/2); 
 
-    
+    point new_circle = {(tmp.x+tmp2.x)/2, (tmp.y+tmp2.y)/2};
+    printf("Center point: x=%f, y=%f\n", new_circle.x, new_circle.y);
+    circle(center_point.x + new_circle.x, center_point.y - new_circle.y, max/2); 
+
 }
-
 
 
 int main() {
     // pencere ayarlanır
-    float en = 400,boy=400;
-    nokta merkez = {en/2, boy/2};
-    initwindow(en, boy, "grafik"); // grafik pencere açıyoruz
+    float width = 400,height=400;
+    point center_point = {width/2, height/2};
+    initwindow(width, height, "grafik"); // grafik pencere açıyoruz
     
-    line(en/2,0, en/2,en); // (200,0)(200,400)
-    line(0,boy/2, en,boy/2); // (0,200)(400,200)
+    // draws + shaped lines in the middle of the window useful for reference
+    line(width/2,0, width/2,width); 
+    line(0,height/2, width,height/2);
 
-    nokta ornek1 = {120, 134};
-    nokta ornek2 = {99, 78};
-    nokta ornek3 = {58, 126};
+    // sample points
+    point ornek1 = {120, 134};
+    point ornek2 = {99, 78};
+    point ornek3 = {58, 126};
     
+    // putting all the points in a list so it's easier to manipulate
+    point liste[3] = {ornek1, ornek2, ornek3};
 
-    nokta liste[3] = {ornek1, ornek2, ornek3};
-
-    int n = sizeof(liste)/sizeof(nokta);
+    // calculates the length of the list
+    int n = sizeof(liste)/sizeof(point);
     printf("Liste boyutu:%d\n", n);
 
-    // noktaları çiziyoruz
+    // draws all the points to the screen
     for(int i=0;i<n;i++)
-        draw_point(liste[i], merkez);
+        draw_point(liste[i], center_point);
     
 
-    hesapla(n, liste, merkez);
+    calculate(n, liste, center_point);
 
 
     while(!kbhit()); //wait for user to press a key 
