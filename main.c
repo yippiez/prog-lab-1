6f3d48d5be7fdf1eab6bcb4b6e86598aa8ed143b
niyazi şahin <niyazisahin3800@gmail.com>
2020-10-22 21:54:49 +0300

    gun 3

--- main.c
+++ main.c
@@ -6,46 +6,92 @@
 // line(x1, y1, x2, y2) x1,y1 ilk noktanın kordinatı x2,y2 inkincinin kordinatı
 
 // nokta verileri bu structa tutulur
+typedef struct Daire{
+    float x;
+    float y;
+    float r;
+} daire;
+
 typedef struct Nokta{
     float x;
     float y;
 } nokta;
 
 // iki nokta arası uzunluğu hesaplar
-double uzunluk(nokta a, nokta b){
+float uzunluk(nokta a, nokta b){
     return sqrt(pow((a.x-b.x),2)+pow((a.y-b.y),2));
 }
 
-// nokta ciz
+daire yeni_daire(nokta a, nokta b, nokta c){
+    // http://www.ambrsoft.com/trigocalc/circle3d.htm
+
+    float A = (a.x*(b.y-c.y))-(a.y*(b.x-c.x))+(b.x*c.y)-(c.x*b.y);
+    float B = (pow(a.x,2)+pow(a.y,2))*(c.y-b.y)+(pow(b.x,2)+pow(b.y,2))*(a.y-c.y)+(pow(c.x,2)+pow(c.y,2))*(b.y-a.y);
+    float C = (pow(a.x,2)+pow(a.y,2))*(b.x-c.x)+(pow(b.x,2)+pow(b.y,2))*(c.x-a.x)+(pow(c.x,2)+pow(c.y,2))*(a.x-b.x);
+    //float D = (pow(a.x,2)+pow(a.y,2))*(c.x*b.y)-(b.x*c.y)+(pow(b.x,2)+pow(b.y,2))*((a.x*c.y)-(c.x*a.y))+(pow(c.x,2)+pow(c.y,2))*((b.x*a.y)-(a.x*b.y));
+    float x = -B/(2*A);
+    float y = -C/(2*A);
+    float r = sqrt(pow(x - a.x, 2) + pow(y - a.y, 2));
+    daire yeni_daire = {x, y, r};
+    return yeni_daire;
+}
+
+int isall_inside(int n, nokta *liste, daire c){
+    return 0;
+}
+
+
+// daire ciz
+void draw_circle(daire c, nokta merkez){
+    circle((merkez.x + c.x), (merkez.y - c.y), c.r);
+}
+
 void draw_point(nokta p, nokta merkez){
     circle((merkez.x + p.x), (merkez.y - p.y), 3);
 }
 
-void hesapla(int n, nokta *liste, nokta merkez){
-    double max=0;
-    nokta tmp;
-    nokta tmp2;
+void hesapla(int n, nokta *liste ,nokta merkez){
+    daire daire_merkez = {0, 0, 0};
 
+    if(n<2)
+        return;
+    
+    // dairelerein ikili ikili kontrolü    
+    daire tmp;
+    float max = 0;
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
-
-            if(i==j)
+            if(j==i)
                 continue;
-
-            double u = uzunluk(liste[i], liste[j]);
+            float u = uzunluk(liste[i], liste[j]);
             if(u>max){
                 max = u;
-                tmp = liste[i];
-                tmp2 = liste[j];
+                tmp.x = (liste[i].x + liste[j].x)/2;
+                tmp.y = (liste[i].y + liste[j].y)/2;
+                tmp.r = max/2;
+                // if(içindeyse)
+                //   daire_merkez = tmp;
             }
         }
     }
-    printf("%lf maks deger.\n", max);
-    nokta daireMerkezi = {(tmp.x+tmp2.x)/2, (tmp.y+tmp2.y)/2};
-    printf("Daire kordinatları: %f, %f\n", daireMerkezi.x, daireMerkezi.y);
-    circle(merkez.x + daireMerkezi.x, merkez.y - daireMerkezi.y, max/2); 
 
+    daire_merkez = tmp;
     
+    // dairelerin üclü üclü kontrolü
+    for(int i=0; i<n; i++){
+        for(int j=0;j<n;j++){
+            for(int k=0;k<n;k++){
+                // eger aynı noktaları kapsıyorsa atla
+                if(i==j || i==k || j==k)
+                    continue;
+                // buraya liste[i] liste[j] liste[k]
+            }
+        }
+    }
+    
+
+    printf("Daire kordinatları: %f, %f\n", daire_merkez.x, daire_merkez.y);
+    draw_circle(daire_merkez, merkez);    
 }
 
 
@@ -59,10 +105,11 @@ int main() {
     line(en/2,0, en/2,en); // (200,0)(200,400)
     line(0,boy/2, en,boy/2); // (0,200)(400,200)
 
-    nokta ornek1 = {120, 134};
-    nokta ornek2 = {99, 78};
-    nokta ornek3 = {58, 126};
+    nokta ornek1 = {10, 10};
+    nokta ornek2 = {21, -12};
+    nokta ornek3 = {-20, 25}; 
     
+    //draw_circle(yeni_daire(ornek1, ornek2, ornek3), merkez);
 
     nokta liste[3] = {ornek1, ornek2, ornek3};
 
@@ -72,7 +119,6 @@ int main() {
     // noktaları çiziyoruz
     for(int i=0;i<n;i++)
         draw_point(liste[i], merkez);
-    
 
     hesapla(n, liste, merkez);
 
